e972a8126f3d665913507b01ed9a6d77e448a553
Apporain <michael.ernst4@gmail.com>
2023-05-10 10:31:42 -0700

    First Commit

---
+++ reacttodolist/src/components/item.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+function Item() {
+    return(
+        <View>
+            <Text>
+                Enter an Item
+            </Text>
+            <Image><a></a></Image>
+            <Image><a></a></Image>
+            <Image><a></a></Image>
+        </View>
+    )
+}
+
+export default Item;

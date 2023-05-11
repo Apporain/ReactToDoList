d0cc38a9709e3db38453da5ae3beef0f9649ca49
Apporain <michael.ernst4@gmail.com>
2023-05-11 10:23:59 -0700

    Initial List Design

--- reacttodolist/src/App.js
+++ reacttodolist/src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
-import Item from './components/item.js'
+import ItemList from './components/itemlist.js'
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ItemList/>
       </header>
     </div>
   );
--- reacttodolist/src/components/item.js
+++ reacttodolist/src/components/item.js
@@ -1,16 +1,49 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { useState } from 'react'
+import './item.css'
 
-function Item() {
+import { ReactComponent as IconPencil } from '../Icons/pencil.svg'
+import {ReactComponent as IconCheck} from '../Icons/check-circle.svg'
+import {ReactComponent as IconCross} from '../Icons/x-circle.svg'
+import {ReactComponent as IconCancel} from '../Icons/x.svg'
+
+
+
+function Item(props) {
+    const [ItemTitle, SetItemTitle] = useState(props.name);
+    const [ItemEdit, SetItemEdit] = useState("readonly");
+    const [ItemCompletion, SetItemCompletion] = useState();
+    const [ItemCancellation, SetItemCancellation] = useState()
+
+   const ActivateEditMode = () => {
+        SetItemEdit("");
+    }
+    const DisableEditMode = () => {
+        SetItemEdit("readonly");
+    }
+
+    const UpdateTextValue = (e) => {
+        SetItemTitle(e.target.value);
+    }
+    /*Implement with state raise
+    const UpdateItemCompletion = () => {
+        SetItemCompletion()
+    }
+
+    const UpdateItemCancellation = () => {
+        SetItemCancellation
+    }*/
+
+    const VarIconPencil = <div class="align-icons d-inline-block" onClick={ActivateEditMode}><IconPencil class="item-icons"/></div>
+    const VarIconCancel = <div class="align-icons d-inline-block" onClick={DisableEditMode}><IconCancel class="item-icons"/></div>
+
     return(
-        <View>
-            <Text>
-                Enter an Item
-            </Text>
-            <Image><a></a></Image>
-            <Image><a></a></Image>
-            <Image><a></a></Image>
-        </View>
+        <div class="border border-dark item-container item-theme-container">
+                <input class="d-inline-block input-text-box"type="text" onChange={UpdateTextValue} value={ItemTitle} readOnly={ItemEdit}></input>
+                    {ItemEdit !== "readonly" ? VarIconCancel : VarIconPencil }
+                    <div class="align-icons d-inline-block" style={{visibility: ItemEdit !== "readonly" ? "hidden" : "visible" }}><IconCheck class="item-icons"/></div>
+                    <div class="align-icons d-inline-block" style={{visibility: ItemEdit !== "readonly" ? "hidden" : "visible" }}><IconCross class="item-icons"/></div>
+        </div>
     )
 }
 
